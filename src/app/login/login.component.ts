0ba5d18a99666f71e8293c313a9fa0e427f954da
Renat Gasanov <ren99999@mail.ru>
2018-12-22 22:55:07 +1000

    Final version of login + redirect to home

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LoginService } from './login.service';
 import { User } from '../models/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,20 +13,26 @@ export class LoginComponent {
 
   public user : User;
 
-  constructor(private loginService: LoginService) {
-  	this.user = new User();
+  constructor(private loginService: LoginService, private router: Router) {
+      this.user = new User();
   }
 
   validateLogin() {
-  	if(this.user.username && this.user.password) {
-  		this.loginService.validateLogin(this.user).subscribe(result => {
+    if(this.user.username && this.user.password) {
+        this.loginService.validateLogin(this.user).subscribe(result => {
         console.log('result is ', result);
+        if(result['status'] === 'success') {
+          this.router.navigate(['/home']);
+        } else {
+          alert('Wrong username/password');
+        }
+
       }, error => {
-        console.log('error is ', error);
+        console.log('Error is: ', error);
       });
-  	} else {
-  		alert('enter user name and password');
-  	}
+    } else {
+        alert('Enter username and password!');
+    }
   }
 
 }
