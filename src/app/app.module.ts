7648d4209d6c49d65697a6d03615a5b38888400b
Renat Gasanov <ren99999@mail.ru>
2018-12-24 20:39:38 +1000

    Popup component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { RootComponent } from './root/root.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ShowPostComponent } from './show-post/show-post.component';
+import { AddPostComponent } from './add-post/add-post.component';
+import { CommonService } from './service/common.service';
+
 
 @NgModule({
   declarations: [
-    RootComponent,
+  	RootComponent,
     LoginComponent,
     HomeComponent,
-    ShowPostComponent
+    ShowPostComponent,
+    AddPostComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +26,7 @@ import { ShowPostComponent } from './show-post/show-post.component';
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [CommonService],
   bootstrap: [RootComponent]
 })
 export class AppModule { }
